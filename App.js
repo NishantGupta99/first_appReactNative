42cd6d197429f225e13175dafd3a900087aa390f
ngactive99 <ngactive99@gmail.com>
2021-10-27 15:51:50 +0530

    Made my initial changes and added a button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+ const [outputText, setOutPutText] = useState('My name is Khan!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change text"  onPress={() => setOutPutText("and i am not a terrorist!")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'coral',
     alignItems: 'center',
     justifyContent: 'center',
   },
